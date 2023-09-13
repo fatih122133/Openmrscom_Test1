97338a62c459202f9c23aae2c8e5bb67afca7435
Enes <enes.buke12@gmail.com>
2023-09-13 22:37:17 +0300

    Case's Update

--- src/RunOperations/Test_Case3.java
+++ src/RunOperations/Test_Case3.java
@@ -1,11 +1,13 @@
 package RunOperations;
 
-public class Test_Case3 {
+import OcElements.OcElements;
+import Utility.BaseDriver;
+import org.testng.annotations.Test;
+
+public class Test_Case3 extends BaseDriver {
+    OcElements oc;
     @Test
     public void Logout() {
-
-        OcElements oc = new OcElements();
-
         //Cikis butonuna tiklar.
         oc.logoutBtn.click();
 
