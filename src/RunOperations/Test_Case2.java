8cefd535787c2d4fe7388d90bfaccb024ee08305
Enes <enes.buke12@gmail.com>
2023-09-13 22:26:22 +0300

    Enes Case's

--- src/RunOperations/Test_Case2.java
+++ src/RunOperations/Test_Case2.java
@@ -1,4 +1,14 @@
 package RunOperations;
 
-public class Test_Case2 {
+import Utility.BaseDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test_Case2 extends BaseDriver {
+    @Test
+    public void Test_Case2(){
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("page"));
+
+    }
 }
